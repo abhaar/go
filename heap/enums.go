ba597f79c007d1247c79abd83aaaa5401c2ad023
abhaar <Lilac@917>
2020-03-08 21:40:06 -0400

    add int heap

---
+++ heap/enums.go
@@ -0,0 +1,8 @@
+package heap
+
+type HeapType int
+
+const (
+	MinHeap HeapType = iota
+	MaxHeap
+)
---
+++ heap/heap_test.go
@@ -0,0 +1,41 @@
+package heap
+
+import (
+	"container/heap"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestMinIntHeap(t *testing.T) {
+	h := NewHeap(MinHeap)
+	heap.Init(h)
+
+	heap.Push(h, 3)
+	heap.Push(h, 2)
+	heap.Push(h, 1)
+	heap.Push(h, 4)
+
+	for h.Len() > 0 {
+		assert.Equal(t, 1, heap.Pop(h))
+		assert.Equal(t, 2, heap.Pop(h))
+		assert.Equal(t, 3, heap.Pop(h))
+		assert.Equal(t, 4, heap.Pop(h))
+	}
+}
+
+func TestMaxIntHeap(t *testing.T) {
+	h := NewHeap(MaxHeap)
+	heap.Init(h)
+
+	heap.Push(h, 3)
+	heap.Push(h, 2)
+	heap.Push(h, 1)
+	heap.Push(h, 4)
+
+	for h.Len() > 0 {
+		assert.Equal(t, 4, heap.Pop(h))
+		assert.Equal(t, 3, heap.Pop(h))
+		assert.Equal(t, 2, heap.Pop(h))
+		assert.Equal(t, 1, heap.Pop(h))
+	}
+}
